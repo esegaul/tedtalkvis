2ba9a85801017ee164a3cb52adbd8f8a51cd279e
evansegaul <evan.segaul@vanderbilt.edu>
2019-11-29 22:35:19 -0500

    stacked bars lookin good

--- stacked_bars.js
+++ stacked_bars.js
@@ -2,7 +2,7 @@ function plot_it()  {
 	var bar_height = 1000;
 	var bar_width = 600;
 	var bar_pad = 50;
-	var row_height = 20;
+	var row_height = 40;
 
 	d3.select('body').append('svg').attr('width', bar_width+bar_pad).attr('height', bar_height);
 
@@ -15,7 +15,7 @@ function plot_it()  {
 	
 	var popularity_scale = d3.scaleLinear()
 		.domain([0, 1])
-		.range(0, bar_width);
+		.range([0, bar_width]);
 	
 	//nest by topic
 	//rollup: for each video in topic, iterate over all tags, filter array to only have those rating
@@ -40,15 +40,71 @@ function plot_it()  {
 		});
 	
 	var stacked_data = topic_stack(topic_nest);
-	console.log(topic_stack(topic_nest));
 
 	
-	d3.select('svg').append('g').attr('id', 'barplot');
+	d3.select('svg').append('g').attr('id', 'barplot')
+
+	var topic_dict = {
+		0: 'Politics',
+		1: 'Music',
+		2: 'Conservation',
+		3: 'Medicine',
+		4: 'World development',
+		5: 'Technology',
+		6: 'Urban Development',
+		7: 'Space',
+		8: 'Education',
+		9: 'Medicine #2'
+	}
+
+	var color_dict = {
+		'Inspiring': 'red',
+		'Funny': 'green',
+		'Informative': 'yellow',
+		'Ingenious': 'blue',
+		'Persuasive': 'orange',
+		'Jaw-dropping': 'purple',
+		'Longwinded': 'brown',
+		'Fascinating': 'lavender',
+		'Courageous': 'lime',
+		'Beautiful': 'navy',
+		'Unconvincing': 'beige',
+		'OK': 'teal',
+		'Confusing': 'pink',
+		'Obnoxious': 'magenta'
+	}
+
+	var color_palette = {'red':'#e6194b',
+						 'green': '#3cb44b',
+						 'yellow': '#ffe119',
+						 'blue': '#4363d8',
+						 'orange': '#f58231',
+						 'purple': '#911eb4',
+						 'teal': '#46f0f0',
+						 'magenta': '#f032e6',
+						 'lime': '#bcf60c', 
+						 'pink': '#fabebe',
+						 'teal': '#008080',
+						 'lavender': '#e6beff', 
+						 'brown': '#9a6324',
+						 'beige': '#fffac8',
+						 'maroon': '#800000',
+						 'mint': '#aaffc3',
+						 'olive': '#808000', 
+						 'apricot': '#ffd8b1', 
+						 'navy': '#000075',
+						 'grey':'#808080',
+						 'white': '#ffffff',
+						 'black': '#000000'
+						};
+
 
 	var topic_bar_groups = d3.select('#barplot').selectAll('empty')
 		.data(stacked_data)
 		.enter()
 		.append('g')
+		.attr('fill', d => color_palette[color_dict[d.key]])
+		.attr('transform', 'translate('+bar_pad+',0)');
 	
 	topic_bar_groups.selectAll('g')
 		.data(d => d)
@@ -57,8 +113,16 @@ function plot_it()  {
 		.attr('y', d => topic_scale(d.data.key))
 		.attr('height', row_height)
 		.attr('x', d => popularity_scale(d[0]))
-		.attr('width', d => popularity_scale(d[1])-popularity_scale(d[0]) )
-		//.attr('length')
+		.attr('width', d => popularity_scale(d[1])-popularity_scale(d[0]));
+
+	d3.select('#barplot').selectAll('empty')
+		.data(topic_nest)
+		.enter()
+		.append('text')
+		.text(d => topic_dict[d.key])
+		.attr('y', d => topic_scale(d.key)+25)
+		.attr("font-size", "15px")
+		.attr('fill', 'black')
 
 
 
