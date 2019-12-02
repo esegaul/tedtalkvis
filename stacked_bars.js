5365ab283bc49386f87c0112ec272b2218ddaf53
evansegaul <evan.segaul@vanderbilt.edu>
2019-12-01 18:56:59 -0600

    fixed bar width

--- stacked_bars.js
+++ stacked_bars.js
@@ -1,7 +1,7 @@
 function plot_it()  {
-	var bar_height = 1000;
-	var bar_width = 600;
-	var bar_pad = 50;
+	var bar_height = 500;
+	var bar_width = 200;
+	var bar_pad = 20;
 	var row_height = 40;
 
 	d3.select('body').append('svg').attr('width', bar_width+bar_pad).attr('height', bar_height);
@@ -10,20 +10,16 @@ function plot_it()  {
 
 	var topic_scale = d3.scaleBand()
 		.domain(Array.from(new Set(ted_talk_data.map(d => d.topic_pred_id))))
-		.range([bar_width, 0])
+		.range([bar_height, 0])
 		.paddingInner(0.2);
 	
 	var popularity_scale = d3.scaleLinear()
 		.domain([0, 1])
 		.range([0, bar_width]);
 	
-	//nest by topic
-	//rollup: for each video in topic, iterate over all tags, filter array to only have those rating
-			//return an object that has ratings and values
-
 	var topic_nest = d3.nest()
 		.key(d => d.topic_pred_id)
-		.rollup(d => { //d represents the array of videos in this topic
+		.rollup(d => { 
 			var dict = {};
 			for(var i=0; i<ratings.length; ++i) {
 				dict[ratings[i]] = (d.filter(video => ratings[i] == video.top_rating).length) / d.length;
@@ -32,7 +28,6 @@ function plot_it()  {
 		})
 		.entries(ted_talk_data);
 
-	
 	var topic_stack = d3.stack()
 		.keys(ratings)
 		.value((d, key) => {
@@ -41,11 +36,10 @@ function plot_it()  {
 	
 	var stacked_data = topic_stack(topic_nest);
 
-	
-	d3.select('svg').append('g').attr('id', 'barplot')
+	d3.select('svg').append('g').attr('id', 'barplot');
 
 	var topic_dict = {
-		0: 'Politics',
+		0: 'Society',
 		1: 'Music',
 		2: 'Conservation',
 		3: 'Medicine',
@@ -55,7 +49,7 @@ function plot_it()  {
 		7: 'Space',
 		8: 'Education',
 		9: 'Medicine #2'
-	}
+	};
 
 	var color_dict = {
 		'Inspiring': 'red',
@@ -72,31 +66,32 @@ function plot_it()  {
 		'OK': 'teal',
 		'Confusing': 'pink',
 		'Obnoxious': 'magenta'
-	}
+	};
 
-	var color_palette = {'red':'#e6194b',
-						 'green': '#3cb44b',
-						 'yellow': '#ffe119',
-						 'blue': '#4363d8',
-						 'orange': '#f58231',
-						 'purple': '#911eb4',
-						 'teal': '#46f0f0',
-						 'magenta': '#f032e6',
-						 'lime': '#bcf60c', 
-						 'pink': '#fabebe',
-						 'teal': '#008080',
-						 'lavender': '#e6beff', 
-						 'brown': '#9a6324',
-						 'beige': '#fffac8',
-						 'maroon': '#800000',
-						 'mint': '#aaffc3',
-						 'olive': '#808000', 
-						 'apricot': '#ffd8b1', 
-						 'navy': '#000075',
-						 'grey':'#808080',
-						 'white': '#ffffff',
-						 'black': '#000000'
-						};
+	var color_palette = {
+		'red':'#e6194b',
+		'green': '#3cb44b',
+		'yellow': '#ffe119',
+		'blue': '#4363d8',
+		'orange': '#f58231',
+		'purple': '#911eb4',
+		'teal': '#46f0f0',
+		'magenta': '#f032e6',
+		'lime': '#bcf60c', 
+		'pink': '#fabebe',
+		'teal': '#008080',
+		'lavender': '#e6beff', 
+		'brown': '#9a6324',
+		'beige': '#fffac8',
+		'maroon': '#800000',
+		'mint': '#aaffc3',
+		'olive': '#808000', 
+		'apricot': '#ffd8b1', 
+		'navy': '#000075',
+		'grey':'#808080',
+		'white': '#ffffff',
+		'black': '#000000'
+	};
 
 
 	var topic_bar_groups = d3.select('#barplot').selectAll('empty')
@@ -122,6 +117,6 @@ function plot_it()  {
 		.text(d => topic_dict[d.key])
 		.attr('y', d => topic_scale(d.key)+25)
 		.attr("font-size", "15px")
-		.attr('fill', 'black')
+		.attr('fill', 'black');
 
 }
