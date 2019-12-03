17f2f3f356dac32c5d00752ad361d87ca21f6d68
Jarrett Perkins <jperkins3503@gmail.com>
2019-12-03 13:16:33 -0600

    remove 2017 - data is incomplete

--- vis.js
+++ vis.js
@@ -15,7 +15,7 @@ function plot_it()  {
 	TOPICS BY YEAR PLOT
 	*
 	*/
-	var year_keys = ["2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017"]
+	var year_keys = ["2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016"]
 
 	var svg = d3.select('body').append('svg').attr('width', 1700).attr('height', 1000).attr('transform', 'translate(5,5)')
 	// group that will contain line plot (id: lines)
